82a2cfe3c8bbcf9c1c028afe00c168c0ca0d28aa
cesare cazzaniga <ccazzaniga@student.ethz.ch>
2024-04-25 11:03:51 +0200

    analysis plots code single Mu

---
+++ plots/analysisPlots_singleMu.sh
@@ -0,0 +1,7 @@
+#CZZ: 2018
+python analysisPlots_singleMu.py --year 2018 --calcMETSig --tuneEra SingleMu_UL_2018_v9  --selection TunegoodIsoMuons18-zeroGoodJetVeto --plot_directory SingleMuALL_UL_2018_v9 
+python analysisPlots_singleMu.py --year 2018 --calcMETSig --tuneEra SingleMu_UL_2018_v9  --selection TunegoodIsoMuons18-zeroGoodJetVeto --normalize --plot_directory SingleMuALL_UL_2018_v9 
+
+
+
+
