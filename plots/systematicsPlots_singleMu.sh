55b17d428a53aa4ff332db9a23e8e6e431b7864c
cesare cazzaniga <ccazzaniga@student.ethz.ch>
2024-04-25 11:00:36 +0200

    singleMu sys plots

---
+++ plots/systematicsPlots_singleMu.sh
@@ -0,0 +1,6 @@
+python systematicsPlots_singleMu.py --year 2018 --plot_directory SingleMuALL_UL_2018_v9 --isChild --selection TunegoodIsoMuons18-zeroGoodJetVeto --selectSys combine
+python systematicsPlots_singleMu.py --year 2018 --plot_directory SingleMuALL_UL_2018_v9 --isChild --selection TunegoodIsoMuons18-zeroGoodJetVeto --selectSys combine --skipJER
+python systematicsPlots_singleMu.py --year 2018 --plot_directory SingleMuALL_UL_2018_v9 --isChild --selection TunegoodIsoMuons18-zeroGoodJetVeto --selectSys combine --skipUnclEn
+python systematicsPlots_singleMu.py --year 2018 --plot_directory SingleMuALL_UL_2018_v9 --isChild --selection TunegoodIsoMuons18-zeroGoodJetVeto --selectSys combine --skipUnclEn --skipJER
+
+
