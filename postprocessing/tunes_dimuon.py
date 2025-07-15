de1f6c281ca19bf216f77ed6e2a18632867c6f13
Cesare Cazzaniga <75684123+cesarecazzaniga@users.noreply.github.com>
2025-07-15 14:29:06 +0200

    removing placeholders

--- postprocessing/tunes_dimuon.py
+++ postprocessing/tunes_dimuon.py
@@ -6,8 +6,8 @@ tuneParams = {
 			#Tune double muon file DATA
 			#Tune double muon file MC
 			"DoubleMuALL_UL_2018_v9": {
-						'data':         [...],
-						'mc':           [...],
+						'data':         [],
+						'mc':           [],
 						'jerData':      'Summer19UL18_JRV2_DATA',
 						'jerMC':        'Summer19UL18_JRV2_MC',
 						'jetThreshold': 15,
